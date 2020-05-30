ff8480404296c90396fa2cc1b63e1a9c5db7ba79
Maciej <maciej.markiewicz@onet.pl>
2020-05-31 01:54:30 +0200

    fix knn and perform tests

--- fashion-mnist/knn.py
+++ fashion-mnist/knn.py
@@ -22,11 +22,17 @@ def classification_error(p_y_x, y_true):
     return sum(y_pred[i] != y_true[i] for i in range(y_true.shape[0])) / len(y_true)
 
 
-def knn_training(X_val, X_train, y_val, y_train, k_values):
+def knn_model_selection(X_val, X_train, y_val, y_train, k_values):
     labels = sort_train_labels_knn(distance_function(X_val, X_train), y_train)
-    probabilities = [p_y_x_knn(labels, k) for k in k_values]
-    errors = [classification_error(prob, y_val) for prob in probabilities]
-    return dict(zip(k_values, errors))
+    prediction = [p_y_x_knn(labels, k) for k in k_values]
+    errors = [classification_error(pred, y_val) for pred in prediction]
+    min_error_index = np.argmin(errors)
+    return dict(zip(k_values, errors)), k_values[min_error_index]
+
+
+def knn_test(X_train, X_test, y_train, y_test, k):
+    labels = sort_train_labels_knn(distance_function(X_test, X_train), y_train)
+    return classification_error(p_y_x_knn(labels, k), y_test)
 
 
 def knn_on_fashion_mnist():
@@ -35,14 +41,21 @@ def knn_on_fashion_mnist():
     train_images = train_images / 255.0
     test_images = test_images / 255.0
 
-    np.reshape(train_images, (60000, 28*28))
-    np.reshape(test_images, (10000, 28*28))
+    train_set = train_images[:1000, :]
+    train_set_labels = train_labels[:1000]
+    val_set = train_images[1000:1200, :]
+    val_set_labels = train_labels[1000:1200]
+    test_set = test_images[:200, :]
+    test_set_labels = test_labels[:200]
 
-    # train_images = train_images[:10000, :]
-    # train_labels = train_labels[:10000]
-    # test_images = test_images[:2000, :]
-    # test_labels = test_labels[:2000]
+    models, best_k = knn_model_selection(val_set, train_set, val_set_labels, train_set_labels, [1, 5, 9])
 
-    res = knn_training(test_images, train_images, test_labels, train_labels, [5])
+    print(models)
+    print(knn_test(train_set, test_set, train_set_labels, test_set_labels, best_k))
 
-    print(res)
+
+# res 10/2k for val set
+# {1: 0.1845, 3: 0.17, 5: 0.1615, 9: 0.1685, 20: 0.183, 50: 0.2065}
+# res for test set 0.1775
+
+# TODO update readme!
